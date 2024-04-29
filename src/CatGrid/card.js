79bd252bb8ff77d34a4e65baed7c8c5873c8f765
Lakshman <lkondini@gmail.com>
2024-04-29 16:15:16 +0530

    fixes

--- src/CatGrid/card.js
+++ src/CatGrid/card.js
@@ -3,7 +3,7 @@ import ReactImage from '../Shared/imageViewer';
 
 export default function Card(props){
     return (
-        <div style={props.style}>
+        <div style={props.style} className='pointer'>
             <p>{props.item.title}</p>
             <ReactImage imgs={ require(`../assets/cat${props.item.position}.jpg`) } />
         </div>
